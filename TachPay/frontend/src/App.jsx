7ed9ca96d571df1461b9a1dd9a3ea5f062ff19ee
tachyonok <tachyonparticle123@gmail.com>
2024-09-24 00:19:49 +0530

    routing react

--- TachPay/frontend/src/App.jsx
+++ TachPay/frontend/src/App.jsx
@@ -1,11 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<Send />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
